1e09f0cad53193a4351adaf6370bd9c617d70f9e
Taylor Huston <taylor.a.huston@gmail.com>
2023-03-23 17:45:53 -0400

    Number of lines function

--- project.js
+++ project.js
@@ -20,4 +20,21 @@ const deposit = () => {
     }
 }
 
+
+const getNumberOfLines = () => {
+    while(true)
+    {
+        const lines = prompt('Enter the number of lines to bet on: ');
+        const numLines = parseFloat(lines);
+
+        if (isNaN(numLines) || numLines <= 0 || numLines > 3) {
+            console.log('Please enter a valid number of lines');
+        } else {
+            return numLines;
+        }
+    }
+}
+
+
 const depositAmount = deposit();
+const numLines = getNumberOfLines();
