c386db9d16784a552b85c7001302bc725a3e7ef7
Taylor Huston <taylor.a.huston@gmail.com>
2023-03-24 13:04:19 -0400

    Implement spin logic

--- project.js
+++ project.js
@@ -6,6 +6,23 @@
 
 const prompt = require('prompt-sync')();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8,
+}
+
+const SYMBOL_VALUES = {
+    "A": 5,
+    "B": 4,
+    "c": 3,
+    "d": 2,
+}
+
 const deposit = () => {
     while(true)
     {
@@ -24,7 +41,7 @@ const deposit = () => {
 const getNumberOfLines = () => {
     while(true)
     {
-        const lines = prompt('Enter the number of lines to bet on: ');
+        const lines = prompt('Enter the number of lines to bet on (1-3): ');
         const numLines = parseFloat(lines);
 
         if (isNaN(numLines) || numLines <= 0 || numLines > 3) {
@@ -35,6 +52,44 @@ const getNumberOfLines = () => {
     }
 }
 
+const getBet = (balance, lines) => {
+    while(true)
+    {
+        const bet = prompt('How much would you like to bet per line? $');
+        const numBet = parseFloat(bet);
 
-const depositAmount = deposit();
+        if (isNaN(numBet) || numBet <= 0 || numBet > balance/lines) {
+            console.log('Please enter a valid bet.');
+        } else {
+            return numBet;
+        }
+    }
+}
+
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+        }
+    }
+    const reels = [];
+    for (let i = 0; i < COLS; i++) {
+        reels.push([]);
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            reels[i].push(reelSymbols[randomIndex]);
+            reelSymbols.splice(randomIndex, 1);
+        }
+    }
+    // console.log(symbols);
+    // console.log(reels);
+    return reels;
+}
+
+const reels = spin();
+//console.log(reels);
+let balance = deposit();
 const numLines = getNumberOfLines();
+const bet = getBet(balance, numLines);
